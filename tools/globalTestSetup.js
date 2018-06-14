507721ded6fcc21231ddef5e0332bf07ef166bf4
Rob Anderson <robert.anderson2@ncl.ac.uk>
2018-06-14 11:03:48 +0100

    :art: Refactored testing mongo to use one string

--- tools/globalTestSetup.js
+++ tools/globalTestSetup.js
@@ -1,6 +1,7 @@
 const MongodbMemoryServer = require('mongodb-memory-server').default
 const mongoose = require('mongoose')
 
-module.exports = function () {
+module.exports = async function () {
   global.__MONGOD__ = new MongodbMemoryServer()
+  global.__MONGO_URI__ = await global.__MONGOD__.getConnectionString()
 }
