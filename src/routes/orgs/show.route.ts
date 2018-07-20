2bab3890052dc9cd4bea00d2170e2d88d668ad84
Rob Anderson <robert.anderson2@ncl.ac.uk>
2018-07-20 14:27:16 +0100

    :art: Removed user embedding of members for now

--- src/routes/orgs/show.route.ts
+++ src/routes/orgs/show.route.ts
@@ -1,6 +1,5 @@
 import { RouteContext, MemberRole } from '@/src/types'
 import { isMongoId } from '@/src/utils'
-import { IOrganisationWithUsers } from '@/src/models'
 
 function makeError (name: string) {
   return `api.orgs.show.${name}`
@@ -25,20 +24,11 @@ export default async ({ req, api, next, models, authJwt }: RouteContext) => {
   
   let result = await models.Organisation.findForUser(user.id)
     .where('_id', req.params.org_id)
-    .populate('members.user', '-fcmToken')
     .limit(1)
   
-  if (result.length === 0) throw makeError('notFound')
+  // Fail if the organisation doesn't exist
+  if (!result[0]) throw makeError('notFound')
   
-  let org: IOrganisationWithUsers = result[0] as any
-  
-  // If you aren't a coordinator, remove user records
-  const isCoordinator = org.members.find(m => m.isActive && m.role === MemberRole.Coordinator)
-  
-  // Remove the users if not a coordinator
-  if (!isCoordinator) {
-    org.members.forEach(m => m.user = m.user.id)
-  }
-  
-  api.sendData(org.toJSONWithActiveMembers())
+  // Send back the formatted organisation
+  api.sendData(result[0].toJSONWithActiveMembers())
 }
