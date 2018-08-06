4464ff96533615410903561346433f8e8f4142d3
Rob Anderson <robert.anderson2@ncl.ac.uk>
2018-08-06 15:54:48 +0100

    :bug: Fixing dependancies and tests bug

--- src/__tests__/models.spec.ts
+++ src/__tests__/models.spec.ts
@@ -6,7 +6,9 @@ describe('#makeModels', () => {
   let connection: Connection
   let models: IModelSet
   beforeAll(async () => {
-    connection = createConnection(process.env.MONGO_URI!)
+    connection = createConnection(process.env.MONGO_URI!, {
+      useNewUrlParser: true
+    })
     models = makeModels(connection)
   })
   afterAll(async () => {
