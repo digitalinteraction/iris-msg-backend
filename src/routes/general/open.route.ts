1fd01808e951afa4b70a390a28282e3752ba9605
Rob Anderson <rob.anderson@ncl.ac.uk>
2018-10-23 11:07:37 +0100

    :art: Refactor open/* to use pug

--- src/routes/general/open.route.ts
+++ src/routes/general/open.route.ts
@@ -1,4 +1,6 @@
 import { RouteContext } from '@/src/types'
+import * as pug from 'pug'
+import { join } from 'path'
 
 function makeError (name: string) {
   return `api.open.${name}`
@@ -7,62 +9,11 @@ function makeError (name: string) {
 const inviteTest = /invite\/.*/
 const donateTest = /donate/
 
-const downloadAppTemplate = (title: string, appLink: string) => `
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width,initial-scale=1">
-  <title>Iris Msg</title>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css">
-  <style>
-  .is-monospace { font-family: "SFMono-Regular",Consolas,"Liberation Mono",Menlo,Courier,monospace; }
-  .is-vertical { vertical-align: middle; }
-  </style>
-</head>
-<body>
-  
-  <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
-    <div class="container">
-      <div class="navbar-brand">
-        <p class="navbar-item">
-          <svg version="1.1" baseProfile="full" width="300" height="36" xmlns="http://www.w3.org/2000/svg">
-            <text class="is-monospace" x="0" y="28" font-size="32" text-anchor="start" font-weight="bold" fill="white">irismsg.io/open</text>
-          </svg>
-        </p>
-      </div>
-    </div>
-  </nav>
-  
-  <div class="hero is-info">
-    <div class="hero-body">
-      <div class="container">
-        <h1 class="title">${title}</h1>
-        <h2 class="subtitle">The open sms donation platform</h2>
-      </div>
-    </div>
-  </div>
-  <section class="section">
-    <div class="container">
-      <div class="notification is-warning is-inline-block">
-        <h1 class="title">❌  Couldn't open app</h1>
-      </div>
-      <p class="subtitle">You have opened a link which should open with <strong>Iris Msg</strong>, but it doesn't look like you have the app installed</p>
-      <h2 class="heading is-size-4"> What to do </h2>
-      <ol class="is-size-5 content">
-        <li>
-          <a class="button is-link is-vertical" href="${appLink}" target="_blank">Download</a>
-          the app
-        </li>
-        <li>
-          <button class="button is-success is-vertical" onclick="window.open(location.href)"> Reload </button>
-          to trigger the link again
-        </li>
-      </ol>
-    </div>
-  </section>
-</body>
-</html>`
+const templatePath = join(__dirname, '../../templates/downloadApp.pug')
+
+const template = process.env.NODE_ENV === 'development'
+  ? (...args: any[]) => pug.compileFile(templatePath)(...args)
+  : pug.compileFile(templatePath)
 
 /* params
  * - 0
@@ -77,9 +28,15 @@ export default async (ctx: RouteContext) => {
 }
 
 export function handleInvite ({ req, res }: RouteContext) {
-  res.send(downloadAppTemplate('Your invitation', process.env.PLAY_STORE_URL!))
+  res.send(template({
+    title: 'Your invitation',
+    appLink: process.env.PLAY_STORE_URL!
+  }))
 }
 
 export function handleDonate ({ req, res }: RouteContext) {
-  res.send(downloadAppTemplate('Donation Request', process.env.PLAY_STORE_URL!))
+  res.send(template({
+    title: 'Donation Request',
+    appLink: process.env.PLAY_STORE_URL!
+  }))
 }
