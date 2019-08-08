583f47ef2a8d4977d2787e5d42aede9997bac8d0
Rob Anderson <rob@andrsn.uk>
2019-08-08 15:09:23 +0100

    :bug: Rework shrink logic around axios bug

--- src/services/links.service.ts
+++ src/services/links.service.ts
@@ -3,6 +3,18 @@ import axios from 'axios'
 const cleanPathStart = (path: string) => path.replace(/^\//, '')
 const cleanPathEnd = (path: string) => path.replace(/\/$/, '')
 
+type ShrunkURL = {
+  id: string
+  createdAt: string
+  updatedAt: string
+  active: boolean
+  creator_id: string
+  long: string
+  public?: string
+  short: string
+  uses: number
+}
+
 export function makeApiUrl(path: string) {
   return cleanPathEnd(process.env.API_URL!) + '/' + cleanPathStart(path)
 }
@@ -26,11 +38,20 @@ export function makeShrinkAgent() {
 
 export async function shrinkLink(longUrl: string): Promise<string> {
   try {
-    const shrinkAgent = canShrink() ? makeShrinkAgent() : null
-    if (!shrinkAgent) throw new Error('Url Shrinker unavailable')
+    // Do nothing if we don't have the environment variables to shrink
+    if (!canShrink()) throw new Error('Url Shrinker unavailable')
 
-    let res = await shrinkAgent.post('/', { url: longUrl })
+    // Create an agent to do the shrinking
+    const agent = axios.create({ baseURL: process.env.SHRINK_URL })
 
+    // Request a shrunken URL
+    const res = await agent.post<ShrunkURL>('/', {
+      token: process.env.SHRINK_KEY,
+      url: longUrl
+    })
+
+    // Return the public url
+    // Fallback to the long url (public may not be set if shrinky is misconfigured)
     return res.data.public || longUrl
   } catch (error) {
     return longUrl
